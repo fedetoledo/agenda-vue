14322c148074efb7c2cc998f70168cc228402697
Federico <toledofederico11@gmail.com>
2020-01-22 19:32:25 -0300

    added tags

---
+++ frontend/src/components/extra/PriorityTag.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-chip small outlined label :color="priorityColorCode">{{verbosePriority}}</v-chip>
+</template>
+
+<script>
+export default {
+
+    props: {
+        priority: {
+            type: Number,
+            required: true
+        } 
+    },
+
+     computed: {
+    priorityColorCode() {
+      return this.priority == 0 ? '#61bd4f': this.priority == 1 ? '#f2d600': '#eb5a46'
+    },
+
+    verbosePriority() {
+      return this.priority == 0 ? 'Low': this.priority == 1 ? 'Middle': 'High'
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/extra/SubjectTag.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-chip dark small label :color="subject.color">{{subject.name}}</v-chip>
+</template>
+
+<script>
+export default {
+
+    props: {
+        subject: {
+            type: Object,
+            required: true
+        } 
+    },
+}
+</script>
+
+<style scoped>
+   
+</style>
