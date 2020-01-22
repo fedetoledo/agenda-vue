4e0b854401354373d57a79243e8d776f73b98985
Federico <toledofederico11@gmail.com>
2020-01-22 19:44:12 -0300

    added main store for general purposes

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,11 +3,13 @@ import Vuex from 'vuex'
 import todos from './modules/todos'
 import subjects from './modules/subjects'
 import exams from './modules/exams'
+import main from './main'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
+        main,
         todos,
         subjects,
         exams
---
+++ frontend/src/store/main.js
@@ -0,0 +1,17 @@
+const getters = {
+
+    getColors: () => {
+        let colors = [
+            {text: 'Pink', value: '#e91e63'},
+            {text: 'Blue', value: '#2196f3'},
+            {text: 'Red', value: '#f44336'},
+            {text: 'Green', value: '#61bd4f'}
+        ]
+        return colors
+    }
+    
+}
+
+export default {
+    getters
+}
